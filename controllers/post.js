0aab95b52a10bd1caaaf15f12a97791f5480416b
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-02-06 20:39:09 -0600

    Added buttons, paths and routes, 50% working

--- controllers/post.js
+++ controllers/post.js
@@ -1,59 +1,61 @@
-const express = require("express");
-const Post = require("../db/schema");
+const express = require('express');
+const Post = require('../db/schema');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  Post.find({})
-    .then(posts => {
-      res.render("index", { posts });
+router.get('/', (req, res) => {
+    Post.find({})
+        .then(posts => {
+            res.render('index', { posts });
+        })
+        .catch(console.error);
+});
+
+router.get('/new', (req, res) => {
+    res.render('new');
+});
+
+router.get('/:id/edit', (req, res) => {
+    Post.findById(req.params.id).then(post => {
+        res.render('edit', post);
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const solved = req.body.solved === 'on' ? true : false;
+    const post = {
+        title: req.body.title,
+        post: req.body.post,
+        solved
+    };
+    Post.findOneAndUpdate({ _id: req.params.id }, post, { new: true }).then(
+        post => {
+            res.redirect('/post');
+        }
+    );
+});
+router.delete('/:id', (req, res) => {
+    Post.findOneAndRemove({ _id: req.params.id }).then(() => {
+        res.redirect('/post');
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            res.render('show', post);
+        })
+        .catch(console.error);
+});
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post: req.body.post
     })
-    .catch(console.error);
-});
-
-router.get("/new", (req, res) => {
-  res.render("new");
-});
-
-router.get("/:id/edit", (req, res) => {
-  Post.findById(req.params.id).then(post => {
-    res.render("edit", post);
-  });
-});
-
-router.put("/:id", (req, res) => {
-  const post = {
-    title: req.body.title,
-    post: req.body.post
-  };
-  Post.findOneAndUpdate({ _id: req.params.id }, post, { new: true }).then(
-    post => {
-      res.redirect("/post");
-    }
-  );
-});
-router.delete("/:id", (req, res) => {
-  Post.findOneAndRemove({ _id: req.params.id }).then(() => {
-    res.redirect("/post");
-  });
-});
-
-router.get("/:id", (req, res) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      res.render("show", post);
-    })
-    .catch(console.error);
-});
-router.post("/", (req, res) => {
-  Post.create({
-    title: req.body.title,
-    post: req.body.post
-  })
-    .then(post => {
-      res.redirect("/post");
-    })
-    .catch(console.error);
+        .then(post => {
+            res.redirect('/post');
+        })
+        .catch(console.error);
 });
 //posts to the index.hbs page, try and get to add to show
 
--- db/schema.js
+++ db/schema.js
@@ -1,11 +1,12 @@
-const mongoose = require("./connection");
+const mongoose = require('./connection');
 
 const PostSchema = new mongoose.Schema({
     title: String,
     author: String,
     post: String,
-    date: String
-})
+    date: String,
+    solved: Boolean
+});
 
 const Post = mongoose.model('Post', PostSchema);
 
